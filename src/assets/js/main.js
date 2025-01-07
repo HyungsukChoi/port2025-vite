07ecbb7457d8a0773dff4a90a3c6a18f86aea8b0
HSCHOI <117467777+HyungsukChoi@users.noreply.github.com>
2025-01-07 14:38:36 +0900

    레이아웃 완성

---
+++ src/assets/js/main.js
@@ -0,0 +1,11 @@
+import {menu} from "./menu.js";
+import {port} from "./menu.js";
+import {link} from "./menu.js";
+import {smooth} from "./menu.js";
+
+window.addEventListener("load", function(){
+    menu();
+    port();
+    link();
+    smooth();
+});
